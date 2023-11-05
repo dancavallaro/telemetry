da0f83c2a4e12b3eed058c85b380dc18fe08dadd
Dan Cavallaro <dancaval@amazon.com>
2023-11-05 14:33:08 -0500

    WIP main app

---
+++ cmd/heartbeats/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/eclipse/paho.mqtt.golang"
+	"log"
+	"os"
+	"time"
+)
+
+var heartbeatHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+const mqttBroker string = "rpi.local:1883"
+const mqttClientId string = "heartbeatmetrics"
+const mqttHeartbeatTopic string = "device/+/heartbeat"
+
+func main() {
+	log.SetPrefix("[heartbeats] ")
+	log.SetFlags(0)
+
+	mqtt.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
+	mqtt.CRITICAL = log.New(os.Stdout, "[CRIT] ", 0)
+	mqtt.WARN = log.New(os.Stdout, "[WARN]  ", 0)
+	//mqtt.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(mqttBroker)
+	opts.SetClientID(mqttClientId)
+	// TODO: Get this out of source control
+	opts.SetUsername("rpi")
+	opts.SetPassword("DHV6x48uBtYI83Ppu0tEWBmH")
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetPingTimeout(1 * time.Second)
+
+	mqttClient := mqtt.NewClient(opts)
+
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		log.Panicln(token.Error())
+	}
+
+	if token := mqttClient.Subscribe(mqttHeartbeatTopic, 0, heartbeatHandler); token.Wait() && token.Error() != nil {
+		log.Fatalln(token.Error())
+	}
+
+	time.Sleep(10 * time.Second)
+
+	mqttClient.Disconnect(1000)
+}
