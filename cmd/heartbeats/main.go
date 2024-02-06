fce9058ce2871733c2eba4841c3f6792fabf7f56
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2024-02-06 14:44:59 -0500

    Add logOnly option to heartbeat listener

--- cmd/heartbeats/main.go
+++ cmd/heartbeats/main.go
@@ -17,8 +17,18 @@ const mqttTopic = "device/+/heartbeat"
 
 var mqttTopicRegex = regexp.MustCompile(`device/(.+)/heartbeat`)
 
+type HeartbeatPublisher interface {
+	PublishHeartbeat(device string) error
+}
+
+type noopPublisher struct{}
+
+func (p noopPublisher) PublishHeartbeat(_ string) error {
+	return nil
+}
+
 type heartbeatHandler struct {
-	publisher heartbeats.CloudwatchPublisher
+	publisher HeartbeatPublisher
 }
 
 func (handler heartbeatHandler) Heartbeat(topic string) {
@@ -52,6 +62,7 @@ var (
 	mqttAddress     = flag.String("mqttAddress", "localhost:1883", "Address:port of MQTT broker")
 	mqttUsername    = flag.String("mqttUsername", "<none>", "MQTT username")
 	mqttPassword    = flag.String("mqttPassword", "<none>", "MQTT password")
+	logOnly         = flag.Bool("logOnly", false, "Log heartbeats, but don't publish metrics to Cloudwatch")
 )
 
 func main() {
@@ -61,12 +72,15 @@ func main() {
 	flag.Parse()
 	printConfigSummary()
 
-	cw := awso.NewClientProvider(func(cfg aws.Config) *cloudwatch.Client {
-		cfg.Region = *region
-		log.Println("Creating new Cloudwatch client")
-		return cloudwatch.NewFromConfig(cfg)
-	})
-	publisher := heartbeats.NewCloudwatchPublisher(&cw, *metricNamespace, *metricName, *metricDimension)
+	var publisher HeartbeatPublisher = noopPublisher{}
+	if !*logOnly {
+		cw := awso.NewClientProvider(func(cfg aws.Config) *cloudwatch.Client {
+			cfg.Region = *region
+			log.Println("Creating new Cloudwatch client")
+			return cloudwatch.NewFromConfig(cfg)
+		})
+		publisher = heartbeats.NewCloudwatchPublisher(&cw, *metricNamespace, *metricName, *metricDimension)
+	}
 
 	log.Printf("Creating MQTT listener for topic '%s'\n", mqttTopic)
 	listener, err := heartbeats.NewMQTTListener(heartbeats.MQTTListenerConfig{
