c2744f058543c379af01b9aade627bd29f3f74a2
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2025-05-25 12:56:53 -0400

    Reimplement metrics exporter as collector

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+build_app() {
+  app="$1"
+  env KO_DOCKER_REPO=ghcr.io/dancavallaro/telemetry ko build \
+    --platform linux/amd64,linux/arm64 \
+    --tags latest,"$(cat "./cmd/$app/version")" \
+    --base-import-paths \
+    "./cmd/$app"
+}
+
+app="${1:-}"
+if [[ -n "$app" ]]; then
+  build_app "$app"
+  exit 0
+fi
+
 go test ./...
 
 for app in heartbeats serial-logger volsync-metrics cluster-heartbeat; do
-  env KO_DOCKER_REPO=ghcr.io/dancavallaro/telemetry ko build \
-    --platform linux/amd64,linux/arm64 \
-    --tags latest,"$(cat ./cmd/$app/version)" \
-    --base-import-paths \
-    ./cmd/$app
+  build_app "$app"
 done
--- cmd/volsync-metrics/main.go
+++ cmd/volsync-metrics/main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"context"
 	"flag"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -11,29 +17,29 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
 )
 
 var (
-	backupSizeBytes = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "volsync_sync_total_bytes",
-			Help: "Size of the latest backup in bytes",
-		},
+	backupSizeMetricDesc = prometheus.NewDesc(
+		"volsync_sync_total_bytes",
+		"Size of the latest backup in bytes",
 		[]string{"namespace", "sourcepvc", "persistentvolume", "storageclass"},
+		nil,
 	)
 
 	// Regex to parse the backup size from logs in the CR status
 	// Matches patterns like "504.129 MiB" or "1.5 GiB"
 	sizeRegex = regexp.MustCompile(`processed \d+ files, ([\d.]+) ((?:[KMGT]i)?B)`)
 )
+
+type volsyncCollector struct {
+	clientset     *kubernetes.Clientset
+	dynamicClient *dynamic.DynamicClient
+}
+
+func (v volsyncCollector) Describe(descs chan<- *prometheus.Desc) {
+	prometheus.DescribeByCollect(v, descs)
+}
 
 func convertToBytes(size float64, unit string) float64 {
 	multipliers := map[string]float64{
@@ -50,72 +56,66 @@ func convertToBytes(size float64, unit string) float64 {
 	return 0
 }
 
-func watchReplicationSources(clientset *kubernetes.Clientset, client dynamic.Interface) {
+func (v volsyncCollector) Collect(metrics chan<- prometheus.Metric) {
 	replicationSource := schema.GroupVersionResource{
 		Group:    "volsync.backube",
 		Version:  "v1alpha1",
 		Resource: "replicationsources",
 	}
 
-	for {
-		list, err := client.Resource(replicationSource).List(context.Background(), metav1.ListOptions{})
-		if err != nil {
-			log.Printf("Error listing ReplicationSources: %v", err)
-			time.Sleep(30 * time.Second)
+	list, err := v.dynamicClient.Resource(replicationSource).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		log.Printf("Error listing ReplicationSources: %v", err)
+		return
+	}
+	log.Printf("Found %d ReplicationSources, updating metrics now", len(list.Items))
+
+	sourcesProcessed := 0
+	for _, item := range list.Items {
+		namespace := item.GetNamespace()
+		name := item.GetName()
+
+		logs, found, err := unstructured.NestedString(item.Object, "status", "latestMoverStatus", "logs")
+		if !found || err != nil {
 			continue
 		}
-		log.Printf("Found %d ReplicationSources, updating metrics now", len(list.Items))
 
-		sourcesProcessed := 0
-		for _, item := range list.Items {
-			namespace := item.GetNamespace()
-			name := item.GetName()
-
-			logs, found, err := unstructured.NestedString(item.Object, "status", "latestMoverStatus", "logs")
-			if !found || err != nil {
-				continue
-			}
-
-			sourcePVC, found, err := unstructured.NestedString(item.Object, "spec", "sourcePVC")
-			if !found || err != nil {
-				continue
-			}
-			pvc, err := clientset.CoreV1().PersistentVolumeClaims(namespace).
-				Get(context.Background(), sourcePVC, metav1.GetOptions{})
-			if err != nil {
-				log.Printf("Couldn't query source PVC %s for ReplicationSource %s/%s: %v", sourcePVC, namespace, name, err)
-				continue
-			}
-
-			matches := sizeRegex.FindStringSubmatch(logs)
-			if len(matches) == 3 {
-				size, err := strconv.ParseFloat(matches[1], 64)
-				if err != nil {
-					continue
-				}
-
-				sizeInBytes := convertToBytes(size, matches[2])
-				backupSizeBytes.WithLabelValues(
-					namespace, sourcePVC, pvc.Spec.VolumeName, *pvc.Spec.StorageClassName,
-				).Set(sizeInBytes)
-				sourcesProcessed++
-			} else {
-				log.Printf("Couldn't parse backup size for ReplicationSource %s/%s from logs", namespace, name)
-			}
+		sourcePVC, found, err := unstructured.NestedString(item.Object, "spec", "sourcePVC")
+		if !found || err != nil {
+			continue
+		}
+		pvc, err := v.clientset.CoreV1().PersistentVolumeClaims(namespace).
+			Get(context.Background(), sourcePVC, metav1.GetOptions{})
+		if err != nil {
+			log.Printf("Couldn't query source PVC %s for ReplicationSource %s/%s: %v", sourcePVC, namespace, name, err)
+			continue
 		}
 
-		log.Printf("Processed %d ReplicationSources successfully, sleeping until next time...", sourcesProcessed)
-		time.Sleep(1 * time.Minute)
+		matches := sizeRegex.FindStringSubmatch(logs)
+		if len(matches) == 3 {
+			size, err := strconv.ParseFloat(matches[1], 64)
+			if err != nil {
+				continue
+			}
+
+			sizeInBytes := convertToBytes(size, matches[2])
+			metrics <- prometheus.MustNewConstMetric(
+				backupSizeMetricDesc, prometheus.GaugeValue, sizeInBytes,
+				namespace, sourcePVC, pvc.Spec.VolumeName, *pvc.Spec.StorageClassName,
+			)
+			sourcesProcessed++
+		} else {
+			log.Printf("Couldn't parse backup size for ReplicationSource %s/%s from logs", namespace, name)
+		}
 	}
+	log.Printf("Processed %d ReplicationSources successfully", sourcesProcessed)
 }
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", "", "path to kubeconfig (optional)")
-	flag.Parse()
-
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 
-	prometheus.MustRegister(backupSizeBytes)
+	kubeconfig := flag.String("kubeconfig", "", "path to kubeconfig (optional)")
+	flag.Parse()
 
 	var config *rest.Config
 	var err error
@@ -137,13 +137,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	go watchReplicationSources(clientset, dynamicClient)
+	prometheus.MustRegister(&volsyncCollector{clientset, dynamicClient})
 
 	log.Println("Starting metrics server at :8080/metrics")
 	http.Handle("/metrics", promhttp.Handler())
