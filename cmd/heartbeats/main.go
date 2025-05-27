3c9970aeff3a4e8c15ffb9fd55aee64e9f054a1d
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2025-05-27 09:33:09 -0400

    Reimplement heartbeats as Prometheus-style metrics server

--- cmd/heartbeats/main.go
+++ cmd/heartbeats/main.go
@@ -2,85 +2,82 @@ package main
 
 import (
 	"flag"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
-	"github.com/dancavallaro/telemetry/pkg/awso"
 	"github.com/dancavallaro/telemetry/pkg/heartbeats"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
-	"os"
-	"os/signal"
+	"net/http"
 	"regexp"
-	"syscall"
+	"sync"
+	"time"
 )
 
-const mqttTopic = "device/+/heartbeat"
+const (
+	heartbeatTTLSeconds = 24 * 60 * 60
+	mqttTopic           = "device/+/heartbeat"
+)
 
-var mqttTopicRegex = regexp.MustCompile(`device/(.+)/heartbeat`)
-
-type HeartbeatPublisher interface {
-	PublishHeartbeat(device string) error
-}
-
-type noopPublisher struct{}
-
-func (p noopPublisher) PublishHeartbeat(_ string) error {
-	return nil
-}
+var (
+	mqttTopicRegex      = regexp.MustCompile(`device/(.+)/heartbeat`)
+	mqttAddress         = flag.String("mqttAddress", "localhost:1883", "Address:port of MQTT broker")
+	mqttUsername        = flag.String("mqttUsername", "<none>", "MQTT username")
+	mqttPassword        = flag.String("mqttPassword", "<none>", "MQTT password")
+	heartbeatMetricDesc = prometheus.NewDesc(
+		"iot_device_last_heartbeat_time",
+		"Time of the last heartbeat received from the device",
+		[]string{"device"}, nil,
+	)
+)
 
 type heartbeatHandler struct {
-	publisher HeartbeatPublisher
+	lastHeartbeats *sync.Map
+}
+
+func newHeartbeatHandler() heartbeatHandler {
+	return heartbeatHandler{
+		lastHeartbeats: &sync.Map{},
+	}
 }
 
 func (handler heartbeatHandler) Heartbeat(topic string) {
 	device := mqttTopicRegex.FindStringSubmatch(topic)[1]
 	log.Printf("Received heartbeat message for device %s\n", device)
-	if err := handler.publisher.PublishHeartbeat(device); err != nil {
-		log.Panic(err)
-	}
+	handler.lastHeartbeats.Store(device, time.Now().Unix())
 }
 
 func (handler heartbeatHandler) Invalid(topic string, message string) {
 	log.Printf("Received invalid heartbeat message on topic '%s': %s\n", topic, message)
 }
 
-func printConfigSummary() {
-	log.Println("============= Configuration =============")
-	log.Printf("MQTT broker address: %s\n", *mqttAddress)
-	log.Printf("MQTT broker username: %s\n", *mqttUsername)
-	log.Printf("AWS region: %s\n", *region)
-	log.Printf("Cloudwatch namespace: %s\n", *metricNamespace)
-	log.Printf("Cloudwatch metric name: %s\n", *metricName)
-	log.Printf("Cloudwatch metric dimension: %s\n", *metricDimension)
-	log.Println("=========================================")
+type heartbeatCollector struct {
+	lastHeartbeats *sync.Map
 }
 
-var (
-	region          = flag.String("region", "us-east-1", "Cloudwatch region to use")
-	metricNamespace = flag.String("metricNamespace", "Testing", "Metric namespace to publish in")
-	metricName      = flag.String("metricName", "Heartbeat", "Metric name to use for heartbeats")
-	metricDimension = flag.String("metricDimension", "Device", "Dimension name to use for identifying devices")
-	mqttAddress     = flag.String("mqttAddress", "localhost:1883", "Address:port of MQTT broker")
-	mqttUsername    = flag.String("mqttUsername", "<none>", "MQTT username")
-	mqttPassword    = flag.String("mqttPassword", "<none>", "MQTT password")
-	logOnly         = flag.Bool("logOnly", false, "Log heartbeats, but don't publish metrics to Cloudwatch")
-)
+func (h heartbeatCollector) Describe(descs chan<- *prometheus.Desc) {
+	prometheus.DescribeByCollect(h, descs)
+}
+
+func (h heartbeatCollector) Collect(metrics chan<- prometheus.Metric) {
+	h.lastHeartbeats.Range(func(key, value any) bool {
+		device := key.(string)
+		lastHeartbeatTime := value.(int64)
+
+		if time.Now().Unix()-lastHeartbeatTime > heartbeatTTLSeconds {
+			log.Printf("Device %s has not sent a heartbeat in the last %d seconds, expiring...", device, heartbeatTTLSeconds)
+			h.lastHeartbeats.Delete(device)
+		} else {
+			metrics <- prometheus.MustNewConstMetric(
+				heartbeatMetricDesc, prometheus.CounterValue, float64(lastHeartbeatTime), device)
+		}
+		return true
+	})
+}
 
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmsgprefix)
 	log.SetPrefix("[heartbeats] ")
 
 	flag.Parse()
-	printConfigSummary()
-
-	var publisher HeartbeatPublisher = noopPublisher{}
-	if !*logOnly {
-		cw := awso.NewClientProvider(func(cfg aws.Config) *cloudwatch.Client {
-			cfg.Region = *region
-			log.Println("Creating new Cloudwatch client")
-			return cloudwatch.NewFromConfig(cfg)
-		})
-		publisher = heartbeats.NewCloudwatchPublisher(&cw, *metricNamespace, *metricName, *metricDimension)
-	}
 
 	log.Printf("Creating MQTT listener for topic '%s'\n", mqttTopic)
 	mqttLogger := log.New(log.Default().Writer(), "[mqtt]", log.Default().Flags())
@@ -97,13 +94,15 @@ func main() {
 		log.Println("Shutting down MQTT listener now...")
 		listener.Close()
 	}()
-	if err := listener.RegisterHandler(mqttTopic, heartbeatHandler{publisher}); err != nil {
+	handler := newHeartbeatHandler()
+	if err := listener.RegisterHandler(mqttTopic, handler); err != nil {
 		log.Panic(err)
 	}
-
-	done := make(chan os.Signal, 1)
-	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
-
 	log.Println("Listening for heartbeats...")
-	<-done
+
+	prometheus.MustRegister(&heartbeatCollector{handler.lastHeartbeats})
+
+	log.Println("Starting metrics server at :8080/metrics")
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
